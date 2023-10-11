f79fb29db4cf5bf7197c4e494ac45106299425ce
dns <dns@LAPTOP-EHRCLL3N>
2023-10-11 21:02:16 +0200

    Добавьте файлы проекта.

---
+++ Lab_rab_2_Odinaev/Program.cs
@@ -0,0 +1,10 @@
+﻿    Console.Write("Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("Введите y: ");
+    double y = double.Parse(Console.ReadLine());
+    Console.WriteLine(x>=-1 && x<=1 && y>=-1 && y<=1 && y>=x+1 && y<=x+1 && y>=-x+1 && y<=x-1 && y<=-x-1);
+
+
+
+
+
